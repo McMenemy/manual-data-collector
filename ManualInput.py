c3560589a7248aa3535107acf6e2e0e2acba1cf4
Suyash Kumar <suyash.kumar@duke.edu>
2014-11-03 22:15:46 -0500

    Added ManualInput File
    ManualInput deals with getting manual data input from the user.

---
+++ ManualInput.py
@@ -0,0 +1,10 @@
+
+'''
+This class deals with getting data input from the user.
+@author Suyash Kumar
+'''
+class ManualInput:
+	outputFileName=""
+	def __init__(self,outputFileName):
+		self.outputFileName=outputFileName
+
