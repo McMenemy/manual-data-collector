12aedbc21484a8c8b1e580ffdaa43e750c0d4eff
McMenemy <jtm35@duke.edu>
2014-11-03 23:43:12 -0500

    Added yn section

--- ManualInput.py
+++ ManualInput.py
@@ -2,7 +2,7 @@
 This class deals with getting manual data input from the user.
 @author Suyash Kumar (suyashkumar) and Josh McMenemy (mcmenemy)
 '''
-import os.path
+
 class ManualInput:
 	outputFileName=""
 	def __init__(self,outputFileName):
@@ -15,40 +15,48 @@ This method asks the user a question with the prompt and of the type specified.
 The type specifier is a string: 
 'Scale10', 'yn' [yes/no], 'OpenResponse', Time, 'intResponse'
 
-Question id--unique id1 for each question
+Question id--unique id for each question
 '''
-	def askQuestion(prompt,typeString, id1):
+	def askQuestion(prompt,typeString, id):
 		if (typeString=='Scale10'):
 			askQuestionScale10(prompt)
+		elif (typeString=='yn')
+			askQuestionyn(prompt)
+
+
+
+
 
 	def askQuestionScale10(prompt):
 		response=input(prompt+'\n[Enter Response as a digit 1-10]')
 		# Check response
-		if (response.isdigit()):
-			#TODO Add methods 
+		while 1
+			if (response.isdigit()):
+				writeQuestionData(prompt, id, repsonse)
+				break
+			else:
+				print "Please enter response as a digit 1-10"
+
+	def askQuestionyn(prompt):
+		response=input(prompt+'\n[Enter Response as yes or no]')
+		# Check response
+		while 1
+			if (response == 'yes' or response == 'no'):
+				writeQuestionData(prompt, id, repsonse)
+				break
+			else:
+				print "Please yes or no lowercase"	
+				
+
+
 
 
 	'''
-	Write a [id].csv file. If exists, add row. 
+	Get the CSV header, figure out if the question has been asked before
+	Header should look like this:
 	prompt @id
 	''' 
-	def writeQuestionData(prompt,id1,response):
-		filename=''.join([id1,'.csv'])
-		# Check if file already exists, if not then write header
-		if !(os.path.isfile(filename)):
-			writeHeader(filename,id1,prompt)
-
-		f=open(filename, 'a') # Open a file for appending 
-		f.write(response)
-		f.close()
-
-	def writeHeader(filename,id1,prompt):
-		f=open(filename,'a')
-		f.write(''.join([prompt,' @',id1]))
-		f.close()
-
-
-
+	def writeQuestionData(prompt,id,response):
 
 
 
