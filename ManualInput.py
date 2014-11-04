7d46b073cb3ecb4af86ee40e6dc7bb994c48c280
Suyash Kumar <suyash.kumar@duke.edu>
2014-11-03 22:31:38 -0500

    Added the Constructor

--- ManualInput.py
+++ ManualInput.py
@@ -1,10 +1,13 @@
 
 '''
-This class deals with getting data input from the user.
+This class deals with getting manual data input from the user.
 @author Suyash Kumar
 '''
 class ManualInput:
 	outputFileName=""
 	def __init__(self,outputFileName):
 		self.outputFileName=outputFileName
+		f=open(outputFileName,'a') # Open file with append specifier
+
+
 
