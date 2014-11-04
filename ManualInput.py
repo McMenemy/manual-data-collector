76b36db3f163c853eec6b83d4024aeb9deeb4abb
McMenemy <jtm35@duke.edu>
2014-11-03 23:44:54 -0500

    yn section

--- ManualInput.py
+++ ManualInput.py
@@ -15,17 +15,34 @@ This method asks the user a question with the prompt and of the type specified.
 The type specifier is a string: 
 'Scale10', 'yn' [yes/no], 'OpenResponse', Time, 'intResponse'
 
-Question id--unique id1 for each question
+Question id--unique id for each question
 '''
-	def askQuestion(prompt,typeString, id1):
+	def askQuestion(prompt,typeString, id):
 		if (typeString=='Scale10'):
 			askQuestionScale10(prompt)
+		elif (typeString=='yn')
+			askQuestionyn(prompt)
+
 
 	def askQuestionScale10(prompt):
 		response=input(prompt+'\n[Enter Response as a digit 1-10]')
 		# Check response
-		if (response.isdigit()):
-			#TODO Add methods 
+		while 1
+			if (response.isdigit()):
+				writeQuestionData(prompt, id, repsonse)
+				break
+			else:
+				print "Please enter response as a digit 1-10"
+
+	def askQuestionyn(prompt):
+		response=input(prompt+'\n[Enter Response as yes or no]')
+		# Check response
+		while 1
+			if (response == 'yes' or response == 'no'):
+				writeQuestionData(prompt, id, repsonse)
+				break
+			else:
+				print "Please yes or no lowercase"
 
 
 	'''
