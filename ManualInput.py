545dd61cea9f5321fb506e58d93977e141f089ac
Suyash Kumar <suyash.kumar@duke.edu>
2014-11-04 00:22:13 -0500

    LOTS OF BUG FIXES
    Corrected syntax/spelling errors got code functional

--- ManualInput.py
+++ ManualInput.py
@@ -5,44 +5,46 @@ This class deals with getting manual data input from the user.
 import os.path
 class ManualInput:
 	outputFileName=""
-	def __init__(self,outputFileName):
+	def __init__(self):
+		outputFileName=""
+		
 		#TODO: adjf;
-		self.outputFileName=outputFileName
+		#self.outputFileName=outputFileName
 		#f=open(outputFileName,'a') # Open file with append specifier
-    
-'''
-This method asks the user a question with the prompt and of the type specified. 
-The type specifier is a string: 
-'Scale10', 'yn' [yes/no], 'OpenResponse', Time, 'intResponse'
+	
+	'''
+	This method asks the user a question with the prompt and of the type specified. 
+	The type specifier is a string: 
+	'Scale10', 'yn' [yes/no], 'OpenResponse', Time, 'intResponse'
 
-Question id--unique id for each question
-'''
-	def askQuestion(prompt,typeString, id):
+	Question id--unique id for each question
+	'''
+	def askQuestion(self,prompt,typeString, id1):
 		if (typeString=='Scale10'):
-			askQuestionScale10(prompt)
-		elif (typeString=='yn')
-			askQuestionyn(prompt)
+			askQuestionScale10(prompt,id1)
+		elif (typeString=='yn'):
+			self.askQuestionYN(prompt,id1)
 
 
-	def askQuestionScale10(prompt):
+	def askQuestionScale10(self,prompt,id1):
 		response=input(prompt+'\n[Enter Response as a digit 1-10]')
 		# Check response
-		while 1
+		while 1:
 			if (response.isdigit()):
-				writeQuestionData(prompt, id, repsonse)
+				self.writeQuestionData(prompt, id1, repsonse)
 				break
 			else:
 				print "Please enter response as a digit 1-10"
 
-	def askQuestionYN(prompt):
-		response=input(prompt+'\n[Enter Response as yes or no]')
+	def askQuestionYN(self,prompt,id1):
+		response1=raw_input(prompt+'\n[Enter Response as y (yes) or n (no)]')
 		# Check response
-		while 1
-			if (response == 'yes' or response == 'no'):
-				writeQuestionData(prompt, id, repsonse)
+		while 1:
+			if (response1 == 'y' or response1 == 'n'):
+				self.writeQuestionData(prompt, id1, response1)
 				break
 			else:
-				print "Please yes or no lowercase"
+				print ""
 
 
 	'''
@@ -50,19 +52,20 @@ Question id--unique id for each question
 	If exists, add row. 
 	prompt @id
 	''' 
-	def writeQuestionData(prompt,id1,response):
+	def writeQuestionData(self,prompt,id1,response):
 		filename=''.join([id1,'.csv'])
 		# Check if file already exists, if not then write header
-		if !(os.path.isfile(filename)):
-			writeHeader(filename,id1,prompt)
+		if not (os.path.isfile(filename)):
+			self.writeHeader(filename,id1,prompt)
 
 		f=open(filename, 'a') # Open a file for appending 
 		f.write(response)
+		f.write('\n') # New Line
 		f.close()
 
-	def writeHeader(filename,id1,prompt):
+	def writeHeader(self,filename,id1,prompt):
 		f=open(filename,'a')
-		f.write(''.join([prompt,' @',id1]))
+		f.write(''.join([prompt,' @',id1,'\n']))
 		f.close()
 
 
---
+++ Tester.py
@@ -0,0 +1,4 @@
+from ManualInput import ManualInput
+
+a=ManualInput()
+a.askQuestion('HI','yn','1')
