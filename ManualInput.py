7d215960dddca6a1b1dace024e7b7a8dc1e241f4
Suyash Kumar <suyash.kumar@duke.edu>
2014-11-03 23:21:25 -0500

    writeQuestionData

--- ManualInput.py
+++ ManualInput.py
@@ -8,14 +8,16 @@ class ManualInput:
 	def __init__(self,outputFileName):
 		#TODO: adjf;
 		self.outputFileName=outputFileName
-		f=open(outputFileName,'a') # Open file with append specifier
+		#f=open(outputFileName,'a') # Open file with append specifier
 
 '''
 This method asks the user a question with the prompt and of the type specified. 
 The type specifier is a string: 
 'Scale10', 'yn' [yes/no], 'OpenResponse', Time, 'intResponse'
+
+Question id--unique id for each question
 '''
-	def askQuestion(prompt,typeString):
+	def askQuestion(prompt,typeString, id):
 		if (typeString=='Scale10'):
 			askQuestionScale10(prompt)
 
@@ -27,8 +29,11 @@ The type specifier is a string:
 
 
 	'''
-	Get the CSV header, figure out if the questions has been asked before
+	Get the CSV header, figure out if the question has been asked before
+	Header should look like this:
+	prompt @id
 	'''
+	def writeQuestionData(prompt,id,response):
 
 
 
