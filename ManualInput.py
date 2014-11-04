d7368038c3fec2b09c9e2c27ab92aafe34a2ae46
Suyash Kumar <suyash.kumar@duke.edu>
2014-11-03 23:41:58 -0500

    Write Question Data

--- ManualInput.py
+++ ManualInput.py
@@ -2,7 +2,7 @@
 This class deals with getting manual data input from the user.
 @author Suyash Kumar (suyashkumar) and Josh McMenemy (mcmenemy)
 '''
-
+import os.path
 class ManualInput:
 	outputFileName=""
 	def __init__(self,outputFileName):
@@ -15,9 +15,9 @@ This method asks the user a question with the prompt and of the type specified.
 The type specifier is a string: 
 'Scale10', 'yn' [yes/no], 'OpenResponse', Time, 'intResponse'
 
-Question id--unique id for each question
+Question id--unique id1 for each question
 '''
-	def askQuestion(prompt,typeString, id):
+	def askQuestion(prompt,typeString, id1):
 		if (typeString=='Scale10'):
 			askQuestionScale10(prompt)
 
@@ -29,11 +29,26 @@ Question id--unique id for each question
 
 
 	'''
-	Get the CSV header, figure out if the question has been asked before
-	Header should look like this:
+	Write a [id].csv file. If exists, add row. 
 	prompt @id
 	''' 
-	def writeQuestionData(prompt,id,response):
+	def writeQuestionData(prompt,id1,response):
+		filename=''.join([id1,'.csv'])
+		# Check if file already exists, if not then write header
+		if !(os.path.isfile(filename)):
+			writeHeader(filename,id1,prompt)
+
+		f=open(filename, 'a') # Open a file for appending 
+		f.write(response)
+		f.close()
+
+	def writeHeader(filename,id1,prompt):
+		f=open(filename,'a')
+		f.write(''.join([prompt,' @',id1]))
+		f.close()
+
+
+
 
 
 
